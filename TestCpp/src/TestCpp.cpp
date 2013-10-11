b0764af2f44651be6bddec64934033019b03b7fb
tshimizu <shimizu.tatsuya@idaj.co.jp>
2013-10-12 08:00:59 +0900

    This is 1st commitment for demo

---
+++ TestCpp/src/TestCpp.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : TestCpp.cpp
+// Author      : TS
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	return 0;
+}
