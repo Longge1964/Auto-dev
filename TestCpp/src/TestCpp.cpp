2732f11002992229d6c38e98c73b2c2496aec277
tshimizu <shimizu.tatsuya@idaj.co.jp>
2013-10-15 17:14:52 +0900

    make ver.1.0 for master branch

--- TestCpp/src/TestCpp.cpp
+++ TestCpp/src/TestCpp.cpp
@@ -12,14 +12,7 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-
-	string data = "add new output!";
-	cout << data << endl;
-
-	for ( int i=0 ; i< 10; i++){
-		cout << "no.:" << i << endl;
-	}
+	cout << "master brach Ver.1.0" << endl; // prints !!!Hello World!!!
 
 	return 0;
 }
